692b9e4db0dc0ae3d97c7a26790425cd50d58862
gyeonghyeonkwon <kyanghyang12@naver.com>
2025-07-01 22:34:11 +0900

    isbn중복 유효성검사

--- src/bookmanager/BookManager.java
+++ src/bookmanager/BookManager.java
@@ -16,9 +16,17 @@ public class BookManager{
   }
   public void addBook() {
     System.out.println("==== 도서 등록 ====");
-    System.out.print("번호 입력: ");
-    int isbn = sc.nextInt();
-    sc.nextLine(); // 버퍼에 남은 개행문자 제거
+    int isbn;
+    while (true) {
+      System.out.print("번호 입력: ");
+      isbn = sc.nextInt();
+      sc.nextLine(); // 버퍼에 남은 개행문자 제거
+      if (isDuplicate(isbn)) {
+        System.out.println("번호를 다시 입력해주세요.");
+        continue;
+      }
+      break;
+    }
 
     System.out.print("제목 입력: ");
     String title= sc.nextLine();
@@ -32,7 +40,17 @@ public class BookManager{
     System.out.print("출판일: ");
     String date= sc.nextLine();
 
+
     Book book = new Book(isbn , title , author , publisher , date);
     books.add(book);
+  }
+
+  private boolean isDuplicate(int isbn) {
+    for (int i = 0; i < books.size(); i++) {
+      if (books.get(i).getIsbn() == isbn) {
+        return true;
+      }
+    }
+    return false;
   }
 }
